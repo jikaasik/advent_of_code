f54c19ba63cd108ad32d773d00d0c12ce9ba3799
Joshua Kaasik <joshua.kaasik@gmail.com>
2023-11-29 16:45:34 -0800

    removed stack trace

--- 2023/python/day_00/test_day_00.py
+++ 2023/python/day_00/test_day_00.py
@@ -2,8 +2,5 @@ import pytest
 from solution import get_input
 
 def test_get_input():
-    try:
-        txt = get_input("input_demo.txt")
-        assert txt == "This is some demo input text"
-    except:
-        import pdb; pdb.set_trace()
+    txt = get_input("input_demo.txt")
+    assert txt == "This is some demo input text"
