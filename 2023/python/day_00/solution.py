093f903642a3df2d6e7519cc52be5139085e758d
Joshua Kaasik <joshua.kaasik@gmail.com>
2023-12-01 11:10:09 -0800

    Completed day 1

--- 2023/python/day_00/solution.py
+++
@@ -1,9 +0,0 @@
-import os 
-
-def get_input(filename):
-    filepath = os.path.join(os.path.dirname(__file__), filename)
-    with open(filepath, "r") as f:
-        return f.read()
-
-txt = get_input("input_demo.txt")
-print(txt)
--- 2023/python/day_00/test_day_00.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from solution import get_input
-
-def test_get_input():
-    txt = get_input("input_demo.txt")
-    assert txt == "This is some demo input text"
