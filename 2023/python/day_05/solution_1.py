b09045d9cbd9d37abc5d26f40dc551ad8be887c8
Joshua Kaasik <joshua.kaasik@gmail.com>
2023-12-04 20:54:32 -0800

    Added basic scaffolding

---
+++ 2023/python/day_05/solution_1.py
@@ -0,0 +1,14 @@
+def main():
+    solutions = []
+
+    for input_type in ['train', 'test']:
+        with open(f"{input_type}_input_1.txt", "r") as f:
+            data = f.read()
+        
+    
+    
+    return solutions
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2023/python/day_05/test_day_05.py
@@ -0,0 +1,15 @@
+import pytest
+from solution_1 import main as part_one
+from solution_2 import main as part_two
+
+
+# def test_part_one():
+#     response = part_one()
+#     assert response[0] == 13
+#     assert response[1] == 23847
+
+
+# def test_part_two():
+#     response = part_two()
+#     assert response[0] == 30
+#     assert response[1] == 8570000
